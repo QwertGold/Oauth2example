815cea731583b318c3bec17e111776184f8af7fd
Klaus Gronbaek <kg@tradable.com>
2017-03-01 22:39:34 +0100

    Added a dumy test to to be able to configure jenkins

---
+++ src/test/java/dk/dindulk/oauth2/DummyTest.java
@@ -0,0 +1,23 @@
+package dk.dindulk.oauth2;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author Klaus Groenbaek
+ */
+public class DummyTest {
+
+    @Test
+    public void success() {
+
+    }
+
+
+    @Test
+    @Ignore
+    public void ignored() {
+
+    }
+
+}
